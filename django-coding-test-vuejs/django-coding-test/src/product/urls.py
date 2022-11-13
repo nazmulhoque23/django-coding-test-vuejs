c80e6c97646623f41f19307dcfc5b92b2f2dd0da
kazi hoque <hoque23kazi@gmail.com>
2022-11-14 03:01:35 +0600

    list, pagination, filter, commit

---
+++ django-coding-test-vuejs/django-coding-test/src/product/urls.py
@@ -0,0 +1,22 @@
+from django.urls import path
+from django.views.generic import TemplateView
+
+from product.views.product import CreateProductView, ProductListView
+from product.views.variant import VariantView, VariantCreateView, VariantEditView
+
+app_name = "product"
+
+urlpatterns = [
+    # Variants URLs
+    path('variants/', VariantView.as_view(), name='variants'),
+    path('variant/create', VariantCreateView.as_view(), name='create.variant'),
+    path('variant/<int:id>/edit', VariantEditView.as_view(), name='update.variant'),
+
+    # Products URLs
+    path('create/', CreateProductView.as_view(), name='create.product'),
+    path('show/', ProductListView.as_view(), name = 'show.product'),
+    #path('list/', TemplateView.as_view(template_name='products/list.html', extra_context={
+     #   'product': True
+    #}), name='list.product'),
+    
+]
---
+++ django-coding-test-vuejs/django-coding-test/src/product/views/product.py
@@ -0,0 +1,53 @@
+from django.views import generic
+from django.db.models import Count,Min, Max
+from django.views.generic import ListView
+from product.models import Variant
+from product.models import ProductVariantPrice, Product, ProductVariant, Variant
+from django.shortcuts import render,redirect
+
+class ProductListView(ListView):
+    template_name = 'products/list.html'
+    queryset = Product.objects.all()
+    paginate_by = 3 
+
+    def get_queryset(self):
+        title = self.request.GET.get('title')
+        variant = self.request.GET.get('variant')
+        price_from = self.request.GET.get('price_from')
+        price_to = self.request.GET.get('price_to')
+        if title != '' and title is not None:
+            new_context = Product.objects.filter(title__icontains = title)
+
+        elif (price_from !='' and price_from is not None)  and (price_to !='' and price_to is not None):
+           
+            new_context = Product.objects.annotate(count = Count('productvariantprice')).filter(count__range=(price_from, price_to))
+        # elif variant != '' and variant is not None:
+        #     variant_context = ProductVariant.objects.filter(variant_title__icontains = variant)
+        #     return variant_context
+
+        else:
+            new_context = Product.objects.all()
+            
+        return new_context 
+
+        
+    def get_context_data(self, **kwargs):
+        context = super().get_context_data(**kwargs)
+
+        context['variant_context'] = ProductVariantPrice.objects.all()
+        context['variant'] = Variant.objects.all()
+        context['product_variant'] = ProductVariant.objects.all()
+        #values('variant__title')
+        return context
+    
+
+class CreateProductView(generic.TemplateView):
+    template_name = 'products/create.html'
+
+    
+    def get_context_data(self, **kwargs):
+        context = super(CreateProductView, self).get_context_data(**kwargs)
+        variants = Variant.objects.filter(active=True).values('id', 'title')
+        context['product'] = True
+        context['variants'] = list(variants.all())
+        return context
